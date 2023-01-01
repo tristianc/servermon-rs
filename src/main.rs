dbe56108cb8de12593fc2e7693ca43703f3fbc07
Tristian Celestin <tristian.celestin@outlook.com>
2023-01-01 14:26:12 -0500

    Can finally ping

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use clap::Parser;
 use log::info;
+use std::net::Ipv4Addr;
 use std::str::FromStr;
+use std::{thread, time};
 use stderrlog::Timestamp;
+
+/// Interval in which servermon will ping the server
+const PERIOD: u64 = 6;
 
 /// Simple program to reboot a server if it is unreachable over the network
 #[derive(Debug, Parser)]
@@ -17,14 +22,15 @@ struct ServermonArgs {
 
     /// Verbosity of output
     #[arg(short, action = clap::ArgAction::Count)]
-    verbosity: usize,
+    verbosity: u8,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = ServermonArgs::parse();
     stderrlog::new()
         .module(module_path!())
-        .verbosity(args.verbosity)
+        .verbosity(usize::from(args.verbosity))
         .timestamp(Timestamp::from_str("sec").unwrap())
         .init()
         .unwrap();
@@ -33,4 +39,17 @@ fn main() {
         "monitoring server {} and smart plug {}",
         args.server, args.smartplug
     );
+
+    let period = time::Duration::from_secs(PERIOD);
+    loop {
+        thread::sleep(period);
+
+        let payload = [0; 8];
+        let (_packet, ping_duration) = surge_ping::ping("192.0.0.1".parse()?, &payload).await?;
+        println!("Ping took {:.3?}", ping_duration);
+
+        info!("server {} is available", args.server);
+    }
+
+    Ok(())
 }
